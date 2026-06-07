8606dce1694fd80345169f5bdd9480e5d1569c06
Mateusz <siiikor.sikorski@gmail.com>
2026-06-07 17:56:36 +0200

    Separate files by domain

--- examples/internal/participant/adapter/basic_grpc.go
+++ examples/internal/participant/adapter/basic_grpc.go
@@ -3,9 +3,7 @@ package adapter
 import (
 	"context"
 
-	"github.com/jackc/pgx/v5/pgxpool"
 	basic "github.com/mat-sik/two-phase-commit-go/examples/internal/generated/basic/v1"
-	transfer "github.com/mat-sik/two-phase-commit-go/examples/internal/generated/transfer/v1"
 	"github.com/mat-sik/two-phase-commit-go/examples/internal/participant"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -59,51 +57,4 @@ func (h *GRPCBasicHandler) RollbackTransaction(ctx context.Context, req *basic.R
 		return nil, status.Error(codes.Internal, err.Error())
 	}
 	return &basic.RollbackTransactionResponse{}, nil
-}
-
-func NewTransferGRPCHandler(pool *pgxpool.Pool) *GRPCTransferHandler {
-	handler := participant.NewTransferTransactionHandler(pool)
-	return &GRPCTransferHandler{
-		transactionPreparer:   handler,
-		transactionCommiter:   handler,
-		transactionRollbacker: handler,
-	}
-}
-
-type GRPCTransferHandler struct {
-	transfer.UnimplementedTransferServiceServer
-
-	transactionPreparer   TransferTransactionPreparer
-	transactionCommiter   TransactionCommiter
-	transactionRollbacker TransactionRollbacker
-}
-
-func (h *GRPCTransferHandler) PrepareTransaction(ctx context.Context, req *transfer.PrepareTransactionRequest) (*transfer.PrepareTransactionResponse, error) {
-	payload := req.GetPayload()
-	transferPayload := participant.TransferPayload{
-		SenderID:   payload.GetSenderId(),
-		ReceiverID: payload.GetReceiverId(),
-		Amount:     payload.GetAmount(),
-	}
-	err := h.transactionPreparer.PrepareTransaction(ctx, req.GetTransactionId(), transferPayload)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-	return &transfer.PrepareTransactionResponse{}, nil
-}
-
-func (h *GRPCTransferHandler) CommitTransaction(ctx context.Context, req *transfer.CommitTransactionRequest) (*transfer.CommitTransactionResponse, error) {
-	err := h.transactionCommiter.CommitTransaction(ctx, req.GetTransactionId())
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-	return &transfer.CommitTransactionResponse{}, nil
-}
-
-func (h *GRPCTransferHandler) RollbackTransaction(ctx context.Context, req *transfer.RollbackTransactionRequest) (*transfer.RollbackTransactionResponse, error) {
-	err := h.transactionRollbacker.RollbackTransaction(ctx, req.GetTransactionId())
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-	return &transfer.RollbackTransactionResponse{}, nil
 }
---
+++ examples/internal/participant/adapter/transfer_grpc.go
@@ -0,0 +1,58 @@
+package adapter
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	pb "github.com/mat-sik/two-phase-commit-go/examples/internal/generated/transfer/v1"
+	"github.com/mat-sik/two-phase-commit-go/examples/internal/participant"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func NewTransferGRPCHandler(pool *pgxpool.Pool) *GRPCTransferHandler {
+	handler := participant.NewTransferTransactionHandler(pool)
+	return &GRPCTransferHandler{
+		transactionPreparer:   handler,
+		transactionCommiter:   handler,
+		transactionRollbacker: handler,
+	}
+}
+
+type GRPCTransferHandler struct {
+	pb.UnimplementedTransferServiceServer
+
+	transactionPreparer   TransferTransactionPreparer
+	transactionCommiter   TransactionCommiter
+	transactionRollbacker TransactionRollbacker
+}
+
+func (h *GRPCTransferHandler) PrepareTransaction(ctx context.Context, req *pb.PrepareTransactionRequest) (*pb.PrepareTransactionResponse, error) {
+	payload := req.GetPayload()
+	transferPayload := participant.TransferPayload{
+		SenderID:   payload.GetSenderId(),
+		ReceiverID: payload.GetReceiverId(),
+		Amount:     payload.GetAmount(),
+	}
+	err := h.transactionPreparer.PrepareTransaction(ctx, req.GetTransactionId(), transferPayload)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &pb.PrepareTransactionResponse{}, nil
+}
+
+func (h *GRPCTransferHandler) CommitTransaction(ctx context.Context, req *pb.CommitTransactionRequest) (*pb.CommitTransactionResponse, error) {
+	err := h.transactionCommiter.CommitTransaction(ctx, req.GetTransactionId())
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &pb.CommitTransactionResponse{}, nil
+}
+
+func (h *GRPCTransferHandler) RollbackTransaction(ctx context.Context, req *pb.RollbackTransactionRequest) (*pb.RollbackTransactionResponse, error) {
+	err := h.transactionRollbacker.RollbackTransaction(ctx, req.GetTransactionId())
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &pb.RollbackTransactionResponse{}, nil
+}
