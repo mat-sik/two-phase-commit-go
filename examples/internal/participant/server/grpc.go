633da6bc5d01049541a2c06a44b8c7c8ba203938
Mateusz <siiikor.sikorski@gmail.com>
2026-07-03 16:08:34 +0200

    Add support for OTel in participant servers

--- examples/internal/participant/server/grpc.go
+++ examples/internal/participant/server/grpc.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"net"
 
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
 )
 
 func NewGRPCServer(registerer GRPCHandlerRegisterer, addr net.Addr) *grpc.Server {
 	server := grpc.NewServer(
+		grpc.StatsHandler(otelgrpc.NewServerHandler()),
 		grpc.UnaryInterceptor(serverAddrInterceptor(addr)),
 	)
 	registerer(server)
