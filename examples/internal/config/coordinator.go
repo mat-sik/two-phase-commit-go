e98e47eb753c9a5e24efed4cf93195597dcdfe8a
Mateusz <siiikor.sikorski@gmail.com>
2026-07-03 12:17:46 +0200

    Add db url for coordinator

--- examples/internal/config/coordinator.go
+++ examples/internal/config/coordinator.go
@@ -7,7 +7,8 @@ import (
 )
 
 type Coordinator struct {
-	Port int `env:"PORT, default=8080"`
+	Port        int    `env:"PORT, default=8080"`
+	DatabaseURL string `env:"DATABASE_URL"`
 }
 
 func NewCoordinator(ctx context.Context) (Collector, error) {
